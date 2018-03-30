5562775a9da0ecd13001417216684fba13e66e5c
Derek Lewis <dlewis1267@gmail.com>
2018-03-29 20:04:42 -0500

    Fixed Web Caesar 2.0

--- main.py
+++ main.py
@@ -9,29 +9,29 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
                 resize:none;
-            }
+            }}
         </style>
     </head>
     <body>
       <form action="/encrypt" method="POST">
-        <label for="user-text">
+        
             Rotate by: <input type="text" name="rot" value="0">
-            <textarea name="text"></textarea>
-            <button type="submit">Submit Query</button>
-        </label>
+            <textarea name="text">{0}</textarea>
+            <input type="submit"/>
+        
       </form>
     </body>
 </html>
@@ -41,15 +41,11 @@ form = """
 def encrypt():
     rot = request.form['rot']
     text = request.form['text']
-    encrypted = ''
-   
-    for char in text:
-        encrypted = encrypted + rotate_string(text, rot)
-        user_content = "<h1>" + encrypted + "</h1>"
-    return user_content  
+
+    return form.format(rotate_string(text, int(rot)))
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 app.run()
